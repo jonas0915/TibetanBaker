2daf9b37b654742af56235e106ea5b3563b609af
jonas0915 <62167665+jonas0915@users.noreply.github.com>
2021-04-09 23:56:47 -0700

    8th commit

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
